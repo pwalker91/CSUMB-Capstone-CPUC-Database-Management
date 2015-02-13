9e188f20f39df3bdff380ebb00c733638f709899
RodofMorad018 <Moradster@Nicholass-MBP.attlocal.net>
2015-02-13 07:16:32 -0800

    Little Updates
    Got insert to work! However I need another eye to look over select and
    update. As I print the query it looks correct but as it executes an
    error occurs. May need help with that for 30 mins or so.

--- Nicks - dbAPI ver 2.py
+++ Nicks - dbAPI ver 2.py
@@ -1,6 +1,14 @@
 class API():
+    config{}
     def __init__(self):
-        self.data = []
+        config["user"] = "Moradster"
+        config["host"] = "localhost"
+        config["password"] = "root"
+        config["database"] = "testdb"
+        config["autocommit"] = True
+    
+    
+    #gets columns to compare
     def _getcolumns(table):
         cursor.execute("SELECT * FROM %s" %table)
         #used to grab column data
@@ -8,11 +16,12 @@ class API():
         #returns the first item (column name) in each tuple
         return [getcolm[0] for getcolm in list]
 
+    #insert works!
     def insert(cursor, table, **kwargs):
-        columnList = _getcolumns(table)
+        """columnList = _getcolumns(table)
         for item in columnList:
             if columnList[item] != kwargs[item]:
-                print ("Exiting")
+                print ("Exiting")"""
         #print (pymysql.Error as err)
         cursor.execute("SELECT * FROM %s" %table)
         query = "INSERT INTO " + table + " ("
@@ -30,10 +39,10 @@ class API():
             print (err)
 
     def select(cursor, table, **kwargs):
-        columnList = _getcolumns(table)
+        """columnList = _getcolumns(table)
         for items in columnList:
             if columnList[items] != kwargs[items]:
-                print ("Exiting")
+                print ("Exiting")"""
         #print (pymysql.Error as err)
         query ="SELECT"
         for keys in kwargs:
@@ -41,13 +50,16 @@ class API():
         query = query[:-1] + "FROM " + table + "WHERE "
         for keys in kwargs:
             query += " %(" + key + ")s,"
-        query = query[:-1]+ ")"
+        query = query[:-1]+ ""
         print (query)
+
+
+
     def update(cursor, table, **kwargs):
-        columnList = _getcolumns(table)
+        """columnList = _getcolumns(table)
         for item in columnList:
             if columnList[item] != kwargs[item]:
-                print ("Exiting")
+                print ("Exiting")"""
         query = "UPDATE " + table + "SET "
         for key in kwargs:
             query += "" + key + ","
@@ -64,5 +76,16 @@ cursor = connect.cursor()
 cursor.execute("SHOW TABLES")
 print ("Please select a table to use")
 table = input('--> ')
+print("Would you like to insert information, query or update table %s?" %table_input)
+iorq = input('i/q/u: ')
+if iorq == 'i' or iorq == 'I':
+    insert_into(table_input, id='6', date='2015-2-5', provider= 'att', speed='4.5')
+if iorq == 'q' or iorq == 'Q':
+    select(cursor, table_input, id, speed='4.5')
+if iorq == 'u' or iorq =='U':
+    update(cursor, table_input, """<orginal column name> = <ori value>, <new/old column> = <new value>""")
+elif iorq != 'q' or iorq != 'i' or iorq != 'u':
+        userflag = False
+
 
 
