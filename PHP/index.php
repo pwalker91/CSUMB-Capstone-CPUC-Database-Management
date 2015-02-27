de0482b9d9bdc7011982d1f689858046461e5f9c
Peter Walker <pwalker@csumb.edu>
2015-02-26 23:11:10 -0800

    Date picker that, on change, gets info from database
    comments at bottom are for my reference

--- PHP/index.php
+++ PHP/index.php
@@ -1,5 +1,7 @@
 <html>
 <head>
+    <meta charset="utf-8">
+    <title>Choose a File by DATE</title>
     <style>
         table, tr
             {   border:1px solid black;
@@ -12,100 +14,135 @@
             {   background-color:#FFFF99
             }
     </style>
+
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.11.3/themes/smoothness/jquery-ui.css">
+    <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+    <script src="//code.jquery.com/ui/1.11.3/jquery-ui.js"></script>
+    <link rel="stylesheet" href="/resources/demos/style.css">
+    <script>
+        $(function() {
+            //Our date picker that will pass the date selected
+            // to the AJAX function
+            $( "#datepicker" ).datepicker({
+                onSelect: function() {
+                    var date = $(this).val().split("/");
+                    var newDate = date[2]+"-"+date[0]+"-"+date[1];
+                    //alert( newDate );
+
+                    $.ajax({
+                        url: './_queryInfo.php',
+                        type: "post",
+                        data: {
+                            'date': newDate
+                        },
+                        cache: false,
+                        success: function(json, status) {
+                            console.log(status);
+                            console.log(json);
+                            //var $title = $('<h1>').text(data.talks[0].talk_title);
+                            var $description = $('<p>').text(json);
+                            $('#info')
+                                .empty()
+                                .append($description);
+                        },
+                        error: function(xhr, desc, err) {
+                            console.log(xhr);
+                            console.log("Details: " + desc + "\nError:" + err);
+                            var $errorInfo = $('<p>').text("Oops. Something went wrong...")
+                            $('#info')
+                                .empty()
+                                .append($errorInfo);
+                        }
+                    });
+                }
+            });
+        });
+    </script>
 </head>
 
 <body>
-    <?php
-    $host = "localhost";
-    $username = "walk4646";
-    $password = "Spiegel42##";
-    $dbname = "walk4646";
+    <div id="selection" style="text-align:center;">
+        <h2>Please choose a date. This will query for all tests conducted on that day</h2>
+        <p>
+            <b>The current possible date ranges are...</b><br>
+            05/26/2014 - 05/30/2014<br>
+            11/03/2014 - 11/07/2014
+            </ul>
+        </p>
+        <p>Date: <input type="text" id="datepicker"></p>
+    </div>
 
-    //create connection
-    $db = new PDO("mysql:host=$host;dbname=$dbname;", $username, $password);
-
-    // Sets Error handling to Exception so it shows ALL errors when trying to get data
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    $idToFind = array(1,2,3,5,10,23,18);
-
-    foreach ($idToFind as $thisID) {
-        echo "<table style=\"border:1px solid black;\">";
-
-        echo "<tr>\n<td></td>";
-            $sql = 'SELECT * FROM Overview
-                    WHERE Overview.id=:ID';
-            $stmt = $db->prepare($sql);
-            $stmt->execute( array (":ID" => $thisID) );
-            $records_found = $stmt->fetchAll();
-            $row = $records_found[0];
-            echo "<td>".$row['id']."</td>".
-                 "<td>".$row["Date"]."</td>".
-                 "<td>".$row["NetworkCarrier"]."</td>".
-                 "<td>".$row["DeviceID"]."</td>".
-                 "<td>".$row["ErrorType"]."</td>\n";
-        echo "</tr>\n";
-
-        echo "<tr>\n".
-             "<th>PING</th>";
-            $sql = 'SELECT * FROM PINGResult
-                    WHERE PINGResult.oid=:ID';
-            $stmt = $db->prepare($sql);
-            $stmt->execute( array (":ID" => $thisID) );
-            $allRows = $stmt->fetchAll();
-            foreach ($allRows as $row) {
-                echo "<td><table><tr>".
-                     "<td>".$row["ConnectionLoc"]."</td>".
-                     "<td>".$row["RTTMin"]."</td>".
-                     "<td>".$row["RTTAverage"]."</td>".
-                     "<td>".$row["RTTMax"]."</td></tr></table>\n";
-            }
-            if (sizeof($allRows)<1) {
-                echo "<td>NO RESULTS</td>";
-            }
-        echo "</tr>\n";
-
-        echo "<tr>\n".
-             "<th>UDP</th>";
-            $sql = 'SELECT * FROM UDPResult
-                    WHERE UDPResult.oid=:ID';
-            $stmt = $db->prepare($sql);
-            $stmt->execute( array (":ID" => $thisID) );
-            $allRows = $stmt->fetchAll();
-            foreach ($allRows as $row) {
-                echo "<td><table><tr>".
-                     "<td>".$row["ConnectionLoc"]."</td>".
-                     "<td>".$row["Time"]."</td>".
-                     "<td>".$row["Jitter"]."</td>".
-                     "<td>".$row["Loss"]."</td></tr></table>\n";
-            }
-            if (sizeof($allRows)<1) {
-                echo "<td>NO RESULTS</td>";
-            }
-        echo "</tr>\n";
-
-        echo "<tr>\n".
-             "<th>TCP</th>";
-            $sql = 'SELECT * FROM TCPResult
-                    WHERE TCPResult.oid=:ID';
-            $stmt = $db->prepare($sql);
-            $stmt->execute( array (":ID" => $thisID) );
-            $allRows = $stmt->fetchAll();
-            foreach ($allRows as $row) {
-                echo "<td><table><tr>".
-                     "<td>".$row["ConnectionLoc"]."</td>".
-                     "<td>".$row["UpSpeed"]."</td>".
-                     "<td>".$row["DownSpeed"]."</td></tr></table>\n";
-            }
-            if (sizeof($allRows)<1) {
-                echo "<td>NO RESULTS</td>";
-            }
-        echo "</tr>\n";
-        echo "</table><br>\n\n";
-    }
-
-    $db = null;
-
-    ?>
+    <div id="info"></div>
 </body>
 </html>
+
+
+
+<?PHP
+/*
+
+
+
+SCRIPT
+$('#action-button').click(function() {
+   $.ajax({
+      url: 'http://api.joind.in/v2.1/talks/10889',
+      data: {
+         format: 'json'
+      },
+      error: function() {
+         $('#info').html('<p>An error has occurred</p>');
+      },
+      dataType: 'jsonp',
+      success: function(data) {
+         var $title = $('<h1>').text(data.talks[0].talk_title);
+         var $description = $('<p>').text(data.talks[0].talk_description);
+         $('#info')
+            .append($title)
+            .append($description);
+      },
+      type: 'GET'
+   });
+});
+
+
+
+
+
+PHP
+$arr = array('a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5);
+echo json_encode($arr);
+
+PHP
+$json = '{"a":1,"b":2,"c":3,"d":4,"e":5}';
+var_dump(json_decode($json));
+var_dump(json_decode($json, true));
+
+NULL returned if JSON cannot be decoded
+
+
+
+
+
+if (isset($_POST['create_submit']))
+{
+    include('/home/CLASSES/walkerpeterj/CST336/.db_conn_default.php');
+
+    $query = "INSERT INTO 336_IP_users (first_name, last_name, username, password, pass_hint, email) "
+            ."VALUES "
+            ."(:first_name, :last_name, :username, :password, :hint, :email)";
+    $values = array( ":first_name" => $_POST['first_name'],
+                     ":last_name" => $_POST['last_name'],
+                     ":username" => $_POST['username'],
+                     ":password" => hash("whirlpool", $_POST['password']),
+                     ":hint" => $_POST['pass_hint'],
+                     ":email" => $_POST['email'] );
+
+    $handle = $db->prepare($query);
+    $handle->execute($values);
+
+    unset($_POST['create_submit']);
+    $db = null;
+    $_SESSION['login_error'] = 'false';
+}*/
+?>
