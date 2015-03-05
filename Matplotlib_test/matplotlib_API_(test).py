a1709df745a215f658927bf8fadacf0da99a9e57
RodofMorad018 <Moradster@Nicholass-MacBook-Pro.local>
2015-03-05 12:13:29 -0800

    Simple Test Working
    Got simple graphing to work

--- Matplotlib_test/matplotlib_API_(test).py
+++ Matplotlib_test/matplotlib_API_(test).py
@@ -8,14 +8,15 @@ conn = pymysql.connect(host="localhost", user="Moradster", passwd="root", db="te
 
 
 # this is the query we will be making
-query = """
-    SELECT Speed, Date
-    FROM
-    WHERE Data >= "2015-01-03"
-    AND Data < "2015-02-25";
-    """
+query = """SELECT Speed, Date FROM test WHERE Date >= "2015-01-03" AND Date < "2015-02-25";"""
 
-df = pandas.read_sql(query, conn, index_col=['Data'])
+df = pandas.read_sql(query, conn, index_col=['Date'])
 fig, ax = plt.subplots()
 df.plot(ax=ax)
+# plt.xticks(TimeStamp, (hour))
+fig.set_size_inches(20.5,10.5)
+plt.grid(True)
+plt.draw()
+fig.savefig('/Users/Moradster/test2.png', dpi=100)
+
 conn.close()
