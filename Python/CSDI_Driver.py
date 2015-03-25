8ee7684590050336eb6fbd7dd613d56181d5ef71
Peter Walker <pwalker@csumb.edu>
2015-03-25 10:43:33 -0700

    More notes for Nick.....
    These notes are how to take a dictionary of arrays, and pass those arrays to statistics functions and store those values.

--- Python/CSDI_Driver.py
+++ Python/CSDI_Driver.py
@@ -10,24 +10,33 @@ import json
 from CSDI_MySQL import CSDI_MySQL as db#talk to them
 #from CSDI_matplotlib_adv.py import CSDI_MPL as mpl
 myDict = {'table':'TCPResults',
-          'criteria': {
+          'table_val':'Throughtput',
+          'file_criteria': {
                        'date': '2015-1-23',
                        'date_operator': '>='
                        },
+          'test_criteria': {
+            'DownSpeed':100
+            'DownSpeed_comparator':"<="
+          },
+          'grouping': "NetworkCarrier",
           'statistics': 'mean'}
 
 #results = db.select(myDict['table']+"Results", **myDict['criteria'])
 if (myDict['table'] == 'TCPResults'):
     query = """SELECT TCPResults.DownSpeed, TCPResults.Id, FileInfo.NetworkCarrier FROM TCPResults JOIN FileInfo ON TCPResults.Oid = FileInfo.Id"""
-    speeds = db.select('TCPResults', "Oid", "DownSpeed", DownSpeed = "0", DownSpeed_operator=">")
-    dates = db.select('FileInfo', "Id", "Date", Date = myDict['date'], Date_operator= myDict['date_operator'])
+    dates = db.select('FileInfo', "*", Date = myDict['file_criteria']['date'], Date_operator= myDict['date_operator'])
+    for ID in dates:
+        speeds = db.select(myDict['table'], "*", Oid=ID, DownSpeed = "0", DownSpeed_operator=">")
+        for value in speeds:
+            values[dates['NetworkCarrier']].append(speeds[myDict['table_val']])
     print(speeds)
 #from here:
 """ 1) do forloop to collect speeds for when
     Oid from "speeds" == Id from "dates"
     2) in same for loop for dates store dates in seperate array where
     Oid == Id
-    
+
     Example:
     for rows in speeds:
     if (dates['Id'] == speeds['Oid'])
@@ -68,7 +77,7 @@ if (myDict['table'] == 'UDPResults'):
 
 """
 After if/else statements
-send data to graph maybe like 
+send data to graph maybe like
 graph(speeds, dates, myDict['statistics'])
 """
 
@@ -76,7 +85,18 @@ graph(speeds, dates, myDict['statistics'])
 
 
 
+VALUES = { "AT&T": [1,45,123,5,1,4,5,54,1,243,5,12,51,23,54,123,5,123,51],
+           "Verizon": [234,5,7,1,23,21,35,5678,6,346,12,12,34,12345,1,23,4,234],
+           "T-Mobile": [1234,5,5123,123,51,256,123,5,1235],
+           "Sprint": [1235,6,1,1,3,6,7,86,346823,46,2134,51,253,512,35]
+           }
 
+VALUES["AT&T"]
+STDEVS = {}
+or
+for key in VALUES:
+    STDEVs[key] = statistics.pstdev(VALUES[key])
+    MEANS[key] = statistics.mean(VALUES[key])
 
 
 
