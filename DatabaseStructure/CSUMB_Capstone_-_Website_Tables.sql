9b1c16fea3f1e2cc494d3a624a0c580ad0bd617a
Peter Walker <pwalker@csumb.edu>
2015-03-26 19:32:39 -0700

    More work on image creation script
    - Slight re-work on Website tables
    - removing old version of image creation driver
    - updates to MySQL API
       - now has a checkConnected() function, checking that we are actually connected
       - better calculation of last insert ID
       - re-ordering of decorator functions
       - slight re-work on generation of query statement
    
    Graphing Driver
    - Gets all pages that have not been generated yet
    - gets all files with criteria, then all tests in file with test criteria
    - TODO: pass values grabbed to graphing function
    - get image location, metadata, and data data (make into JSON)
    - update PageRequest row, and insert into PageResults table

--- DatabaseStructure/CSUMB_Capstone_-_Website_Tables.sql
+++ DatabaseStructure/CSUMB_Capstone_-_Website_Tables.sql
@@ -8,57 +8,39 @@
 
 -- tables
 -- Table HelpRequest
-CREATE TABLE HelpRequest (
-    Id smallint    NOT NULL ,
-    InsertTimestamp timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP ,
-    Name varchar(50)    NULL ,
-    Email varchar(50)    NOT NULL ,
-    Subject varchar(30)    NOT NULL ,
-    Message blob    NOT NULL ,
-    PhoneNumber varchar(20)    NULL ,
-    LastPageVisited varchar(50)    NOT NULL ,
-    CONSTRAINT HelpRequest_pk PRIMARY KEY (Id)
-) ENGINE INNODB
-;
+CREATE TABLE `HelpRequest` (
+  `Id` smallint(6) NOT NULL AUTO_INCREMENT,
+  `InsertTimestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `Name` varchar(50) DEFAULT NULL,
+  `Email` varchar(50) NOT NULL,
+  `Subject` varchar(30) NOT NULL,
+  `Message` blob NOT NULL,
+  `PhoneNumber` varchar(20) DEFAULT NULL,
+  `LastPageVisited` varchar(50) NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Table Query
-CREATE TABLE Query (
-    Id int    NOT NULL ,
-    InsertTimestamp timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP ,
-    AnalysisOpts blob    NOT NULL ,
-    ContactEmail varchar(50)    NOT NULL ,
-    ContactName varchar(50)    NULL ,
-    IsGenerated bool    NOT NULL ,
-    ErrorEncountered bool    NOT NULL ,
-    CONSTRAINT Query_pk PRIMARY KEY (Id)
-) ENGINE INNODB
-;
+CREATE TABLE `PageRequest` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `InsertTimestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `AnalysisOpts` blob NOT NULL,
+  `ContactEmail` varchar(50) NOT NULL,
+  `ContactName` varchar(50) DEFAULT NULL,
+  `IsGenerated` tinyint(1) NOT NULL,
+  `ErrorEncountered` tinyint(1) NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Table Results
-CREATE TABLE Results (
-    Id int    NOT NULL ,
-    CalculatedData blob    NOT NULL ,
-    ImagePath varchar(100)    NOT NULL ,
-    MetaInfo blob    NOT NULL ,
-    PageHash varchar(10)    NOT NULL ,
-    CONSTRAINT Results_pk PRIMARY KEY (Id)
-) ENGINE INNODB
-;
-
-
-
-
-
--- foreign keys
--- Reference:  FK_0 (table: Results)
-
-
-ALTER TABLE Results ADD CONSTRAINT FK_0 FOREIGN KEY FK_0 (Id)
-    REFERENCES Query (Id)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE;
-
+CREATE TABLE `PageResults` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `CalculatedData` blob NOT NULL,
+  `ImagePath` varchar(100) NOT NULL,
+  `MetaInfo` blob NOT NULL,
+  `PageHash` varchar(10) NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 -- End of file.
-
