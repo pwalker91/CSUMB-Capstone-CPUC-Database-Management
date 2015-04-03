80c230db76f376e150db7fd7bb3db51829a9a7c8
RodofMorad018 <Moradster@Nicholass-MacBook-Pro.local>
2015-04-03 13:47:16 -0700

    Working Contacts and Verify page
    Got reCaptcha to work and be required. Simplistic model so far.
    
    Kilroy was here……
    And Peter Walker was here too he does not approve this message

--- PHP/nickspages/contacts.php
+++ PHP/nickspages/contacts.php
@@ -1,71 +1,68 @@
-<?php
-$host = "localhost";
-//this changes from user to user
-$dbname ="mora3237";
-//
-$username = "mora3237";
-//
-$password = "secret";
-//establishes connection to the MySQL database
-$dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-$dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//used to establish which table the we will be querying specifically
-//just incase its too hard to ask user to select table
-//but that shouldn't be hard
-$sql = "USE TABLE Overview";
-$stmt = $dbConn -> prepare($sql);
-$stmt -> execute ();
-$result = $stmt -> fetchAll();
 
-?>
-<!doctype html>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<meta charset="UTF-8">
-<title>Work In Progress.</title>
+  <meta charset="utf-8">
+
+  <!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame 
+       Remove this if you use the .htaccess -->
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+  <title>Contacts</title>
+  <meta name="description" content="">
+  <meta name="author" content="Nicholas Moradi">
+
+  <meta name="viewport" content="width=device-width; initial-scale=1.0">
+
+  <!-- Replace favicon.ico & apple-touch-icon.png in the root of your domain and delete these references -->
+  <link rel="shortcut icon" href="/favicon.ico">
+  <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+  <script src='https://www.google.com/recaptcha/api.js'></script>
 </head>
 
 <body>
-	<form>
-		Carriers:
-		<br />
-		<select>
-			<option>All</option>
-			<option>Verizon</option>
-			<option>T-Mobile</option>
-			<option>Sprint</option>
-			<option>AT&T</option>
+  <div>
+    <header>
+      <h1>Help/Contact</h1>
+    </header>
+    <nav>
+      <p><a href="/">Home</a></p>
+      <p><a href="/contact">Contact</a></p>
+    </nav>
 
-		</select>
-		Location:
-		<br />
-		<select>
-			<option>Both</option>
-			<option>West</option>
-			<option>East</option>
+    <div>
+      <form method="post" action="verify.php">
+      	<fieldset style = "width: 500px; margin:  0px auto;">
+      		<legend>Help/Contact Page</legend>
+      		Name: <input type='text' name='Name' placeholder='Enter Name' required> <br />
+			Email: <input type='text'name='email' placeholder='Enter Email' required> <br />
+			Subject Matter
+			<select name = 'subject'>
+				<option value="Data_Error"> Data Error </option>
+    			<option value="Graph_Error"> Graph Error </option>
+    			<option value="Website_Bug"> Website Bug </option>
+    			<option value="Other"> Other </option>
+    			 
+			</select>
+			<br />
+			<textarea name="Message" rows="6" cols="50">
+				Enter Message here
+			</textarea>
+			<br />
+			<?php
+    			 require_once('recaptchalib.php');
+     			 $publickey = "6LcXzgQTAAAAAKSUbd_RVGpAdxDKiXD4SEpn9nrK"; // you got this from the signup page
+    			 echo recaptcha_get_html($publickey);
+  			?>
+			<br />
+			<input type="submit" value="Submit">
+      	</fieldset>
+      </form>
+    </div>
 
-		</select>
-		Type of Connection:
-		<br />
-		<select>
-			<option>All</option>
-			<option>UDP</option>
-			<option>TCP</option>
-
-		</select>
-		Connection Speeds:
-		<select>
-			<option>All</option>
-			<option>DownSpeed</option>
-			<option>UpSpeed</option>
-		</select>
-		Statistics:
-		<select>
-			<option>None</option>
-			<option>Mean</option>
-			<option>Median</option>
-			<option>Max</option>
-		</select>
-	</form>
+    <footer>
+     <p>&copy; Copyright  by Nicholas Moradi</p>
+    </footer>
+  </div>
 </body>
 </html>
